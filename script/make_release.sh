7c5965777a92a6d5bd764b558f9d2ec8c2739bd6
Antoine Grondin <antoinegrondin@gmail.com>
2023-01-07 15:42:59 +0900

    try more stuff

--- script/make_release.sh
+++ script/make_release.sh
@@ -1,11 +1,36 @@
 #!/usr/bin/env bash
 
-set -eu
+set -eux
 
 root=$(git rev-parse --show-toplevel)
 
-function main() {
-    apictl
+owner=humanlogio
+name=apictl
+tag=0.1.0
+
+function list_archive_filenames() {
+    jq < dist/artifacts.json -r '.[] | select(.type == "Archive") | .name'
 }
 
-main
+function get_archive_url() {
+    read filename
+    gh api graphql -F owner=${owner} -F name=${name} -F tag=${tag} -F filename=${filename} -f query='
+        query($name: String!, $owner: String!, $tag: String!, $filename: String!) {
+            repository(name: $name, owner: $owner) {
+                release(tagName: $tag) {
+                    releaseAssets(first: 1, name: $filename) {
+                        nodes {
+                            downloadUrl
+                        }
+                    }
+                }
+            }
+        }' --jq '.data.repository.release.releaseAssets.nodes[0].downloadUrl'
+}
+
+function main() {
+    list_archive_filenames | get_archive_url | cat
+    
+}
+
+main 
